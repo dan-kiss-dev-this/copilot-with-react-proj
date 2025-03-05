13a9ccbc24d80ef8b7814f32aa0823ca50289595
Daniel Kiss <flowfast47@gmail.com>
2025-03-05 12:51:04 -0800

    adjust component layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Home from './Home';
-import Register from './Register';
 
 function App() {
   const [count, setCount] = useState(0);
-
   const incrementCount = () => setCount((count) => count + 1);
-  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </Router>
-      <div onClick={incrementCount}>Count is {count}</div>
+      <p onClick={incrementCount}>Count is {count}</p>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import './App.css'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+// import Home from './Home';
+import Register from './Register';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 )
